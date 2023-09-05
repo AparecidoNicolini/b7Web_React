647c813ce7b22c0f3d3ddc2f1ad96b8b19251157
Aparecido Nicolini <aparecido.nicolini@hotmail.com>
2023-09-05 15:52:57 -0300

    Atualização Context...

---
+++ context11/src/components/OnlineUsers.tsx
@@ -0,0 +1,10 @@
+import { CountContext } from "@/contexts/CountContext"
+import { useContext } from "react"
+
+export const OnlineUsers = () => {
+    const count = useContext(CountContext)
+
+    return(
+        <div>1</div>
+    )
+}
